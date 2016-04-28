edb81506d91f6714e914c0ba8c2c1961039c40c8
Michael Carroll <michaelcarroll@Zipcoders-MacBook-Pro-4.local>
2016-04-27 21:16:25 -0400

    testing 123 testing 123

---
+++ src/main/java/io/michaelcarroll/App.java
@@ -0,0 +1,9 @@
+package io.michaelcarroll;
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/io/michaelcarroll/Calculator.java
@@ -0,0 +1,27 @@
+package io.michaelcarroll;
+
+public class Calculator {
+
+
+    public int sum(int value1, int value2){
+
+        return value1 + value2;
+    }
+
+
+    public int subtract(int value1, int value2) {
+
+        return value1 - value2;
+    }
+
+    public int multiply(int value1, int value2) {
+
+        return value1 * value2;
+    }
+
+
+    public int divide(int value1, int value2) {
+
+        return value1/value2;
+    }
+}
